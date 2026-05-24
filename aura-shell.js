5110b30db5f30a49d66d597aece95edb67892ab7
Sergiosaac <elparche.foodpopayan@gmail.com>
2026-05-24 20:31:33 +0000

    feat: agregar página Comunidad + enlazar desde sidebar

--- aura-shell.js
+++ aura-shell.js
@@ -247,6 +247,7 @@
   else if (pageIs('dashboard.html'))                             { leftActive = 'dashboard'; }
   else if (pageIs('examen-ascenso'))                             { leftActive = 'examen'; rightActive = 'examen'; }
   else if (pageIs('tienda.html'))                                { leftActive = 'tienda'; }
+  else if (pageIs('comunidad.html'))                             { leftActive = 'comunidad'; }
   else if (pageIs('settings.html'))                              { leftActive = 'settings'; }
   if      (pageIs('movies.html') || pageIs('play-movies.html')) { rightActive = 'movies'; }
   else if (pageIs('lyriclab.html'))                              { rightActive = 'lyriclab'; }
@@ -307,7 +308,7 @@
     slBtn('dashboard', 'Dashboard', D.dash,    'dashboard.html') +
     slBtn('ranking',   'Ranking',   D.ranking, '') +
     slBtn('examen',    t('nav_exam'),      D.examen,  'examen-ascenso.html') +
-    slBtn('comunidad', t('nav_community'), D.comuni,  '') +
+    slBtn('comunidad', t('nav_community'), D.comuni,  'comunidad.html') +
     slBtn('tienda',    t('nav_store'),     D.tienda,  'tienda.html') +
     '<div class="aura-sl-spacer"></div>' +
     slBtn('uilang',    t('nav_ui_lang'),   D.globe,   null) +
