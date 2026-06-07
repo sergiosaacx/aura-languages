d1b45dbefe9b87b2451cbafd4eab6ddf84047f51
sergiosaacx <creatorspremium.co@gmail.com>
2026-06-06 20:01:54 -0500

    fix(shell): dashboard-mobile.css?v=3

--- aura-shell.js
+++ aura-shell.js
@@ -48,7 +48,7 @@
     var _pageCssMap = [
       ['play-movies', 'play-movies-mobile.css'],
       ['home',        'home-mobile.css'],
-      ['dashboard',   'dashboard-mobile.css'],
+      ['dashboard',   'dashboard-mobile.css?v=3'],
       ['movies',      'movies-mobile.css'],
       ['lyriclab',    'lyriclab-mobile.css'],
       ['settings',    'settings-mobile.css'],
