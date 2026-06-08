b6bb18904c7c98f9a3f71f924cdd5a1340d822c5
sergiosaacx <creatorspremium.co@gmail.com>
2026-06-08 15:09:20 -0500

    chore: bump cache aura-v26

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
                 Cache-first para imágenes/fuentes (estáticos)
    ============================================================ */
 
-const CACHE_NAME = 'aura-v25';
+const CACHE_NAME = 'aura-v26';
 const CACHE_STATIC = 'aura-static-v9';
 
 // Assets que cacheamos de inmediato al instalar
