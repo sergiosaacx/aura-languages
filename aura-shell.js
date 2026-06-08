6ae075f4f8f2e96ec41a04d010bc26ac8e65162e
Sergiosaac <elparche.foodpopayan@gmail.com>
2026-06-08 01:09:20 +0000

    feat: add shadowlab-mobile.css + register in aura-shell

--- aura-shell.js
+++ aura-shell.js
@@ -53,6 +53,7 @@
       ['lyriclab',    'lyriclab-mobile.css'],
       ['flashcards',  'flashcards-mobile.css'],
       ['collocations', 'collocations-mobile.css'],
+      ['shadowlab',   'shadowlab-mobile.css'],
       ['settings',    'settings-mobile.css'],
     ];
     for (var _pi = 0; _pi < _pageCssMap.length; _pi++) {
